b6cea9e9eeb4edd3fe13cbf1311d0b184a71ecfb
罗祖邦 <luozubang@wondersgroup.com>
2019-05-04 21:39:23 +0800

    测试自动化部署

---
+++ src/main/java/com/arryluo/testjenkins/Test.java
@@ -0,0 +1,14 @@
+package com.arryluo.testjenkins;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test {
+@RequestMapping
+    @ResponseBody
+    public Object test(){
+    return "测试Jenkins自动化部署";
+}
+}
